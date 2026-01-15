5bda7a26951a1d5aac985fa03ad45b382872ad21
Skq <skq.rain@gmail.com>
2026-01-15 08:07:05 -0500

    add suport for fade terrain

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -11,9 +11,9 @@ using System.Linq;
 [assembly: System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace HotPalette;
+namespace PaletteReload;
 
-[BepInPlugin("seko.hotpalette", "Palette Hot Reload", "0.4")]
+[BepInPlugin("seko.PaletteReload", "Palette Reload", "0.4")]
 sealed class ModPlugin : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
@@ -32,7 +32,7 @@ sealed class ModPlugin : BaseUnityPlugin
         if (isInit) return;
         isInit = true;
 
-        Logger.LogMessage("[HotPalette] Loaded!");
+        Logger.LogMessage("[PaletteReload] Loaded!");
 
         On.RoomCamera.Update += RoomCamera_Update;
         On.DevInterface.TerrainPanel.GetPalettes += DevInterface_TerrainPanel_GetPalettes;
@@ -52,12 +52,14 @@ sealed class ModPlugin : BaseUnityPlugin
         return list;
     }
 
-    static bool changedThisFrame = true;
+    static bool changedThisFramePalette = true;
+    static bool changedThisFrameTerrain = true;
 
     
     FileSystemWatcher watcherA;
     FileSystemWatcher watcherB;
     FileSystemWatcher watcherTerrainA;
+    FileSystemWatcher watcherTerrainB;
 
     static readonly HashSet<int> pendingReloads = new();
     static readonly HashSet<string> pendingTerrainReloads = new();
@@ -65,7 +67,7 @@ sealed class ModPlugin : BaseUnityPlugin
     int lastA = 0;
     int lastB = 0;
     string lastTerrainA = "";
-
+    string lastTerrainB = "";
 
     private void RoomCamera_Update(On.RoomCamera.orig_Update orig, RoomCamera self)
     {
@@ -75,44 +77,49 @@ sealed class ModPlugin : BaseUnityPlugin
             // A del tool thing a guess
             return;
         }
-        var sw = Stopwatch.StartNew();
+        //var sw = Stopwatch.StartNew();
 
-        UpdateWatchers(self.paletteA, self.paletteB, self.terrainPalette.MainPaletteName);
+        UpdateWatchers(self.paletteA, self.paletteB, self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
         TryReloadPalette(self, self.paletteA, ref self.fadeTexA);
         TryReloadPalette(self, self.paletteB, ref self.fadeTexB);
         TryReloadTerrainPalette(self, self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
 
-        if (changedThisFrame)
+        if (changedThisFramePalette)
         {
             self.ApplyFade();
-
-            self.terrainPalette = new TerrainPalette(self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
-            self.ReloadTerrainPalette();
-
-            self.terrainPalette = new TerrainPalette(self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
-            self.ReloadTerrainPalette();
-
-            changedThisFrame = false;
+            changedThisFramePalette = false;
         }
 
-        sw.Stop();
-        UnityEngine.Debug.Log($"[HotPalette] Frame reload check: {sw.Elapsed.TotalMilliseconds} ms");
+        if (changedThisFrameTerrain)
+        {
+            // Two times to fix a strange bug that reload the before palette
+            // Idk why, but just i do this and fixed it?????
+            self.terrainPalette = new TerrainPalette(self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
+            self.ReloadTerrainPalette();
+
+            self.terrainPalette = new TerrainPalette(self.terrainPalette.MainPaletteName, self.terrainPalette.FadePaletteName);
+            self.ReloadTerrainPalette();
+            changedThisFrameTerrain = false;
+        }
+
+        //sw.Stop();
+        //UnityEngine.Debug.Log($"[PaletteReload] Frame reload check: {sw.Elapsed.TotalMilliseconds} ms");
     }
 
     
 
-    void UpdateWatchers(int palA, int palB, string terrainA)
+    void UpdateWatchers(int palA, int palB, string terrainA, string terrainB)
     {
         if (palA != lastA)
         {
             lastA = palA;
-            ConfigureWatcher(ref watcherA, palA);
+            ConfigurPaletteWatcher(ref watcherA, palA);
         }
 
         if (palB != lastB)
         {
             lastB = palB;
-            ConfigureWatcher(ref watcherB, palB);
+            ConfigurPaletteWatcher(ref watcherB, palB);
         }
 
         if (terrainA != lastTerrainA)
@@ -120,6 +127,39 @@ sealed class ModPlugin : BaseUnityPlugin
             lastTerrainA = terrainA;
             ConfigureTerrainWatcher(ref watcherTerrainA, terrainA);
         }
+
+        if (terrainB != lastTerrainB)
+        {
+            lastTerrainB = terrainB;
+            ConfigureTerrainWatcher(ref watcherTerrainB, terrainB);
+        }
+    }
+
+    void ConfigurPaletteWatcher(ref FileSystemWatcher watcher, int id)
+    {
+        // Destroy previous watcher
+        if (watcher != null)
+        {
+            watcher.EnableRaisingEvents = false;
+            watcher.Dispose();
+            watcher = null;
+        }
+
+        string path = AssetManager.ResolveFilePath($"Palettes/palette{id}.png", true, false);
+        if (!File.Exists(path))
+        {
+            Logger.LogWarning($"[PaletteReload] Palette {id} not found.");
+            return;
+        }
+
+        watcher = new FileSystemWatcher();
+        watcher.Path = Path.GetDirectoryName(path);
+        watcher.Filter = Path.GetFileName(path);
+        watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size;
+        watcher.Changed += (s, e) => pendingReloads.Add(id);
+        watcher.EnableRaisingEvents = true;
+
+        //Logger.LogMessage($"[PaletteReload] Observing palette {id} → {path}");
     }
 
     private void ConfigureTerrainWatcher(ref FileSystemWatcher watcherTerrainA, string terrainA)
@@ -135,7 +175,7 @@ sealed class ModPlugin : BaseUnityPlugin
         string path = AssetManager.ResolveFilePath($"terrainpalettes/{terrainA}.png", true, false);
         if (!File.Exists(path))
         {
-            Logger.LogWarning($"[HotPalette] Terrain palette {terrainA} not found.");
+            Logger.LogWarning($"[PaletteReload] Terrain palette {terrainA} not found.");
             return;
         }
 
@@ -146,42 +186,20 @@ sealed class ModPlugin : BaseUnityPlugin
         watcherTerrainA.Changed += (s, e) => pendingTerrainReloads.Add(terrainA);
         watcherTerrainA.EnableRaisingEvents = true;
 
-        //Logger.LogMessage($"[HotPalette] Observing terrain palette → {path}");
-    }
-
-    void ConfigureWatcher(ref FileSystemWatcher watcher, int id)
-    {
-        // Destroy previous watcher
-        if (watcher != null)
-        {
-            watcher.EnableRaisingEvents = false;
-            watcher.Dispose();
-            watcher = null;
-        }
-
-        string path = AssetManager.ResolveFilePath($"Palettes/palette{id}.png", true, false);
-        if (!File.Exists(path))
-        {
-            Logger.LogWarning($"[HotPalette] Palette {id} not found.");
-            return;
-        }
-
-        watcher = new FileSystemWatcher();
-        watcher.Path = Path.GetDirectoryName(path);
-        watcher.Filter = Path.GetFileName(path);
-        watcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.Size;
-        watcher.Changed += (s, e) => pendingReloads.Add(id);
-        watcher.EnableRaisingEvents = true;
-
-        //Logger.LogMessage($"[HotPalette] Observing palette {id} → {path}");
+        //Logger.LogMessage($"[PaletteReload] Observing terrain palette → {path}");
     }
 
     private void TryReloadTerrainPalette(RoomCamera cam, string mainPaletteName, string fadePaletteName)
     {
         if (pendingTerrainReloads.Remove(mainPaletteName))
         {
-            UnityEngine.Debug.Log($"[HotPalette] Reloading terrain palette {mainPaletteName}");
-            changedThisFrame = true;
+            changedThisFrameTerrain = true;
+            UnityEngine.Debug.Log($"[PaletteReload] Reloading terrain palette {mainPaletteName}");
+        }
+        if (pendingTerrainReloads.Remove(fadePaletteName))
+        {
+            changedThisFrameTerrain = true;
+            UnityEngine.Debug.Log($"[PaletteReload] Reloading terrain fade palette {fadePaletteName}");
         }
     }
 
@@ -190,7 +208,8 @@ sealed class ModPlugin : BaseUnityPlugin
         if (pendingReloads.Remove(palID)) 
         {
             LoadPalette(cam, palID, ref fadeTex);
-            changedThisFrame = true;
+            changedThisFramePalette = true;
+            UnityEngine.Debug.Log($"[PaletteReload] Reloading palette {palID}");
         }
     }
 
